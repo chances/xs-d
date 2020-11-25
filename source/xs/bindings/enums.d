f770e2c906825b0439358487b224bfd1da9c945c
Chance Snow <git@chancesnow.me>
2020-11-25 16:18:09 -0600

    Ensure enum members show up in the docs

--- source/xs/bindings/enums.d
+++ source/xs/bindings/enums.d
@@ -38,37 +38,64 @@ enum JSType{
 
 ///
 enum JSError {
+  ///
   noError = 0,
+  ///
   unknownError = 1,
+  ///
   evalError = 2,
+  ///
   rangeError = 3,
+  ///
   referenceError = 4,
+  ///
   syntaxError = 5,
+  ///
   typeError = 6,
+  ///
   uriError = 7,
+  ///
   errorCount = 8,
 }
 
 ///
 enum MachineError {
-	debuggerExit = 0,
+	///
+  debuggerExit = 0,
+  ///
   notEnoughMemoryExit = 1,
+  ///
   stackOverflowExit = 2,
+  ///
   fatalCheckExit = 3,
+  ///
   deadStripExit = 4,
+  ///
   unhandledExceptionExit = 5,
+  ///
   noMoreKeysExit = 6,
+  ///
   tooMuchComputationExit = 7,
 }
 
 /// The attributes of a property.
 enum Attribute : xsAttribute {
+  /// Specifies that a property has no special attributes.
+  /// See_Also: `PropertyAttributes.none`
 	default_ = 0,
+  /// Specifies that the delete operation should fail on a property.
+  /// See_Also: `JSObject.deleteProperty`
 	dontDelete = 2,
+  /// Specifies that a property should not be enumerated by property enumerators and JavaScript `for...in` loops.
 	dontEnum = 4,
+  /// Specifies that a property is read-only.
 	dontSet = 8,
+  /// Specifies that a property is static.
 	static_ = 16,
+  /// Specifies that a property is a getter Function.
 	isGetter = 32,
+  /// Specifies that a property is a setter Function.
 	isSetter = 64,
+  ///
 	changeAll = 30
 }
